e4b51b8c8225e3d0bcf64862a02e945cec696615
XD <>
2026-04-22 12:02:32 +0200

    add

---
+++ New Folder/lab3.X/adc.c
@@ -0,0 +1,13 @@
+/*
+ * File:   adc.c
+ * Author: local
+ *
+ * Created on April 22, 2026, 11:47 AM
+ */
+
+
+#include "xc.h"
+
+int main(void) {
+    return 0;
+}
---
+++ New Folder/lab3.X/newmainXC16.c
@@ -0,0 +1,13 @@
+/*
+ * File:   newmainXC16.c
+ * Author: local
+ *
+ * Created on April 22, 2026, 11:43 AM
+ */
+
+
+#include "xc.h"
+
+int main(void) {
+    return 0;
+}
