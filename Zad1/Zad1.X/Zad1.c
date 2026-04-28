b26c302a6a29464ba57c5557b01b1e819be81329
PawelRzadkowski <147385702+PawelRzadkowski@users.noreply.github.com>
2026-04-28 21:04:03 +0200

    change

--- Zad1/Zad1.X/Zad1.c
+++ Zad1/Zad1.X/Zad1.c
@@ -28,109 +28,100 @@
 
 
 int main(void) {
- //   AD1PCFG = 0xFFFF;
-    TRISA = 0x0000;   
+    AD1PCFG = 0xFFFF;
+    TRISA = 0x0000;
     TRISD = 0xFFFF;
-    
+ 
     unsigned char zmiennaStan = 0;
-    int rd6prev = 0;
-    int rd13prev = 0;
     unsigned char licznik = 0;
-    
-    
-    while(1){
-        int buttonrd6 = PORTDbits.RD6;
-        int buttonrd13 = PORTDbits.RD13;
-        //zmiana stanu 
-        if(buttonrd6 == 1 && rd6prev ==0){
-            zmiennaStan++;
-            licznik = 0;
-            if(zmiennaStan > 8){
-                zmiennaStan = 0;
-            }
-        }
-        
-        if(buttonrd13 == 1 && rd13prev == 0){
-            if(zmiennaStan == 0){
-                zmiennaStan = 8;
-            }
-            else{
-                zmiennaStan--;
-            }
-            
-        }
-        
-        rd6prev = buttonrd6;
-        rd13prev = buttonrd13;
-        
-        switch(zmiennaStan){
+ 
+    char prev6 = 0, current6 = 0;
+    char prev7 = 0, current7 = 0;
+ 
+    int pos = 0;
+    int kierunek = 1;
+    unsigned char kolejka = 0x01;
+    unsigned char lfsr = 0b111001;
+ 
+    while (1) {
+ 
+        switch (zmiennaStan) {
             case 0:
                 LATA = licznik;
                 licznik++;
-                __delay32(10000);
                 break;
             case 1:
                 LATA = licznik;
                 licznik--;
-                __delay32(10000);
                 break;
             case 2:
                 LATA = licznik ^ (licznik >> 1);
                 licznik++;
-                __delay32(10000);
                 break;
             case 3:
                 LATA = licznik ^ (licznik >> 1);
                 licznik--;
-                __delay32(10000);
                 break;
-            case 4:{
+            case 4: {
                 unsigned char dziesiatki = licznik / 10;
                 unsigned char jednosci = licznik % 10;
                 LATA = (dziesiatki << 4) | jednosci;
                 licznik++;
-                if(licznik > 99) licznik = 0;
-                __delay32(10000);
+                if (licznik > 99) licznik = 0;
                 break;
             }
-            case 5:{
+            case 5: {
                 unsigned char dziesiatki = licznik / 10;
                 unsigned char jednosci = licznik % 10;
                 LATA = (dziesiatki << 4) | jednosci;
-                if(licznik == 0) licznik = 99;
+                if (licznik == 0) licznik = 99;
                 else licznik--;
-                __delay32(10000);
                 break;
             }
-            case 6:{
-                static int pos = 0;
-                static int kierunek = 1;
+            case 6:
                 LATA = (1 << pos);
                 pos += kierunek;
-                if(pos == 7 || pos == 0) kierunek *= -1;
-                __delay32(10000);
+                if (pos == 7 || pos == 0) kierunek *= -1;
                 break;
-            }
-            case 7:{
-                static unsigned char kolejka = 0x01;
+            case 7:
                 LATA = kolejka;
                 kolejka <<= 1;
-                if(kolejka == 0) kolejka = 0x01;
-                __delay32(10000);
+                if (kolejka == 0) kolejka = 0x01;
                 break;
-            }
-            case 8:{
-                static unsigned char lfsr = 0b111001;
+            case 8: {
                 unsigned char bit = ((lfsr >> 5) ^ (lfsr >> 4)) & 1;
                 lfsr = (lfsr << 1) | bit;
                 LATA = lfsr;
-                __delay32(10000);
                 break;
             }
         }
+ 
+        prev6  = PORTDbits.RD6;
+        prev7 = PORTDbits.RD7;
+ 
+        __delay32(100000);
+ 
+        current6 = PORTDbits.RD6;
+        current7 = PORTDbits.RD7;
+ 
+        if (current6 - prev6 == 1) {
+            zmiennaStan++;
+            if (zmiennaStan > 8) zmiennaStan = 0;
+            licznik = 0;
+            pos = 0; kierunek = 1;
+            kolejka = 0x01;
+            lfsr = 0b111001;
+        }
+ 
+        if (current7 - prev7 == 1) {
+            if (zmiennaStan == 0) zmiennaStan = 8;
+            else zmiennaStan--;
+            licznik = 0;
+            pos = 0; kierunek = 1;
+            kolejka = 0x01;
+            lfsr = 0b111001;
+        }
     }
-    
+ 
     return 0;
-    
-    
 }
