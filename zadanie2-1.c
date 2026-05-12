1f4c6fb9f744b74f04d392a85b745be77fe4fe2a
PawelRzadkowski <147385702+PawelRzadkowski@users.noreply.github.com>
2026-05-12 19:20:08 +0200

    add

--- zadanie2-1.c
+++ zadanie2-1.c
@@ -1,3 +1,5 @@
+
+
 // CONFIG2
 #pragma config POSCMOD = XT
 #pragma config OSCIOFNC = ON
@@ -19,98 +21,139 @@
 #include "libpic30.h"
 #include "adc.h"
 
-#define LICZBA_PROGRAMOW   2
-#define OPOZNIENIE_DEBOUNCE 40000UL
+#define LICZBA_PROGRAMOW 2
 
 static const unsigned long TABELA_PREDKOSCI[5] = {
-    2000000UL,
-    6000000UL,
-    12000000UL,
-    24000000UL,
-    40000000UL
+    100000UL,
+    400000UL,
+    1000000UL,
+    3000000UL,
+    7000000UL
 };
 
-static unsigned char licznik   = 0;
-static int           pozycja   = 0;
-static int           kierunek  = 1;
+static unsigned char licznik = 0;
 
-void inicjalizacja(void) {
+static int pozycja = 0;
+static int kierunek = 1;
+
+void inicjalizacja(void)
+{
     ADC_SetConfiguration(ADC_CONFIGURATION_DEFAULT);
     ADC_ChannelEnable(ADC_CHANNEL_POTENTIOMETER);
-    TRISA  = 0x0000;
-    TRISB |= (1 << 3) | (1 << 4);
-    LATA   = 0x0000;
+    TRISA = 0x0000;
+    TRISD |= (1 << 6);
+    TRISD |= (1 << 13);
+    LATA = 0x0000;
 }
 
-unsigned long pobierzOpoznienie(void) {
-    unsigned long odczyt = ADC_Read10bit(ADC_CHANNEL_POTENTIOMETER);
-    if (odczyt == 0xFFFF) return TABELA_PREDKOSCI[2];
-    unsigned char poziom = (unsigned char)(odczyt / 205);
-    if (poziom > 4) poziom = 4;
+unsigned long pobierzOpoznienie(void)
+{
+    unsigned long odczyt;
+    unsigned char poziom;
+
+    odczyt = ADC_Read10bit(ADC_CHANNEL_POTENTIOMETER);
+
+    if (odczyt == 0xFFFF)
+        return TABELA_PREDKOSCI[2];
+
+    poziom = (unsigned char)(odczyt / 205);
+
+    if (poziom > 4)
+        poziom = 4;
+
     return TABELA_PREDKOSCI[poziom];
 }
 
-void resetujStan(void) {
-    licznik  = 0;
-    pozycja  = 0;
+void resetujStan(void)
+{
+    licznik = 0;
+
+    pozycja = 0;
     kierunek = 1;
 }
 
-void licznik_binarny_gora(unsigned long opoznienie) {
+void program1(void)
+{
     LATA = licznik;
     licznik++;
-    __delay32(opoznienie);
 }
 
-void wezyk(unsigned long opoznienie) {
-    LATA = (unsigned int)(0b111 << pozycja);
+void program2(void)
+{
+    LATA = (0b111 << pozycja);
+
     pozycja += kierunek;
-    if (pozycja == 5 || pozycja == 0)
-        kierunek *= -1;
-    __delay32(opoznienie);
-}
 
-unsigned char wykryjWcisniecie(unsigned char aktualny, unsigned char *poprzedni) {
-    unsigned char wcisniety = 0;
-    if (*poprzedni == 1 && aktualny == 0) {
-        __delay32(OPOZNIENIE_DEBOUNCE);
-        wcisniety = 1;
+    if (pozycja >= 5) {
+        pozycja = 5;
+        kierunek = -1;
     }
-    *poprzedni = aktualny;
-    return wcisniety;
+
+    if (pozycja <= 0) {
+        pozycja = 0;
+        kierunek = 1;
+    }
 }
 
-int main(void) {
+int main(void)
+{
     inicjalizacja();
 
     unsigned char aktywny_program = 0;
-    unsigned char poprzedni_rb3 = 1;
-    unsigned char poprzedni_rb4 = 1;
+
+    char prev6 = 0;
+    char curr6 = 0;
+
+    char prev13 = 0;
+    char curr13 = 0;
+
     unsigned long opoznienie;
 
     while (1) {
+
         opoznienie = pobierzOpoznienie();
 
-        if (wykryjWcisniecie(PORTBbits.RB3, &poprzedni_rb3)) {
-            aktywny_program = (aktywny_program + 1) % LICZBA_PROGRAMOW;
-            resetujStan();
-        }
-
-        if (wykryjWcisniecie(PORTBbits.RB4, &poprzedni_rb4)) {
-            aktywny_program = (aktywny_program + LICZBA_PROGRAMOW - 1) % LICZBA_PROGRAMOW;
-            resetujStan();
-        }
-
         switch (aktywny_program) {
+
             case 0:
-                licznik_binarny_gora(opoznienie);
+                program1();
                 break;
+
             case 1:
-                wezyk(opoznienie);
+                program2();
                 break;
+
             default:
                 aktywny_program = 0;
                 break;
+        }
+
+        prev6 = PORTDbits.RD6;
+        prev13 = PORTDbits.RD13;
+
+        __delay32(opoznienie);
+
+        curr6 = PORTDbits.RD6;
+        curr13 = PORTDbits.RD13;
+
+        if (prev6 == 1 && curr6 == 0) {
+
+            aktywny_program++;
+
+            if (aktywny_program >= LICZBA_PROGRAMOW)
+                aktywny_program = 0;
+
+            resetujStan();
+        }
+
+        if (prev13 == 1 && curr13 == 0) {
+
+            if (aktywny_program == 0)
+                aktywny_program = LICZBA_PROGRAMOW - 1;
+            else
+                aktywny_program--;
+
+            resetujStan();
         }
     }
 
